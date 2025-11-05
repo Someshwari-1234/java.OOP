9282f71c28f18cea95247b85fe8e125980164937
Someshwari <someshwariwarwatte@gmail.com>
2025-11-05 23:56:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,33 @@
 class Employee {
     String name;
+    int id;
     double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
     void work() {
         System.out.println(name + " is working.");
     }
+
+    void showDetails() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name       : " + name);
+        System.out.println("Salary     : " + salary);
+    }
 }
 
-class Manager extends Employee {   // Manager IS-A Employee
+// Manager IS-A Employee
+class Manager extends Employee {
     int teamSize;
+
+    Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary); // calling parent constructor
+        this.teamSize = teamSize;
+    }
 
     void conductMeeting() {
         System.out.println(name + " is conducting a meeting with " + teamSize + " team members.");
@@ -17,6 +36,11 @@ class Manager extends Employee {   // Manager IS-A Employee
 
 public class Main {
     public static void main(String[] args) {
-        Manager m = new Manager();
-        m.name = "Someshwari";
-        m
+
+        Manager m = new Manager("Someshwari", 101, 50000, 8);
+
+        m.showDetails();       // inherited from Employee
+        m.work();              // inherited from Employee
+        m.conductMeeting();    // Manager's own method
+    }
+}
