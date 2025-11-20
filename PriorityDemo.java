13c5f1fee1750531a2d2457913bf2c97d1fc6f09
Someshwari <someshwariwarwatte@gmail.com>
2025-11-20 23:42:35 +0530

    Create PriorityDemo.java

---
+++ PriorityDemo.java
@@ -0,0 +1,16 @@
+public class PriorityDemo extends Thread {
+    public void run() {
+        System.out.println("Thread running → " + getName());
+    }
+
+    public static void main(String[] args) {
+        PriorityDemo t1 = new PriorityDemo();
+        PriorityDemo t2 = new PriorityDemo();
+
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
