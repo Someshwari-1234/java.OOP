ad7f5cf749080903ce4f8e3d65a6be53d29178b0
Someshwari <someshwariwarwatte@gmail.com>
2025-11-21 23:16:52 +0530

    Implement thread pool demo with fixed thread pool

---
+++ ThreadPoolDemo.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            int task = i;
+            service.submit(() -> {
+                System.out.println("Task " + task + " executed by " + Thread.currentThread().getName());
+            });
+        }
+
+        service.shutdown();
+    }
+}
